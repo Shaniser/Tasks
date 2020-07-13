e16e0faecfb957bfb7463e8c87e3a6a5e8c82d74
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-07-13 19:05:46 +0300

    main menu dev start

--- app/src/main/java/com/godelsoft/tasks/MainActivity.kt
+++ app/src/main/java/com/godelsoft/tasks/MainActivity.kt
@@ -2,10 +2,29 @@ package com.godelsoft.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainCard.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.nextCategoryBtn -> {
+
+            }
+            R.id.prevCategoryBtn -> {
+
+            }
+            R.id.mainCard -> {
+                Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
