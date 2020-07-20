4f1efdf6299bd05db3f48f3aff6484544087a68f
Nikita <nikitavolkov!707@gmail.com>
2020-07-20 15:07:38 +0300

    Teacher class added

---
+++ app/src/main/java/com/godelsoft/tasks/Teacher.kt
@@ -0,0 +1,10 @@
+package com.godelsoft.tasks
+
+data class Teacher(
+    var name: String,
+    var email: String? = null,
+    var number: String? = null,
+    var department: String? = null
+){
+
+}
