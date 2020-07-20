6bc37ed4d970f32fb94fddba1326a7606f8b3243
VolkovNik <nikitavolkov1707@gmail.com>
2020-07-20 15:18:38 +0300

    Teacher added Lesson corrected

--- app/src/main/java/com/godelsoft/tasks/Lesson.kt
+++ app/src/main/java/com/godelsoft/tasks/Lesson.kt
@@ -6,8 +6,8 @@ class Lesson(
     date: String,
     timeBegin: String,
     timeEnd: String,
-    var classroom: String,
-    var teacher: String? = null // TODO сделать класс препод (почта и тд)
+    var classroom: String? = null,
+    var teacher: Teacher? = null
 
 ) : Event(header, content, date, timeBegin, timeEnd) {
 
---
+++ app/src/main/java/com/godelsoft/tasks/Teacher.kt
@@ -0,0 +1,10 @@
+package com.godelsoft.tasks
+
+data class Teacher (
+    var name: String,
+    var email: String? = null,
+    var phoneNumber: String? = null,
+    var department: String? = null
+){
+
+}
