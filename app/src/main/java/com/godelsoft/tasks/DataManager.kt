94a0846d2178f049713d19338792c92bd5c4e6eb
573pn01v01k <litovchenkop.n@gmail.com>
2020-08-30 11:40:08 +0300

    parse lib add

--- app/src/main/java/com/godelsoft/tasks/DataManager.kt
+++ app/src/main/java/com/godelsoft/tasks/DataManager.kt
@@ -1,5 +1,7 @@
 package com.godelsoft.tasks
 
+
+import com.beust.klaxon.Klaxon
 import com.godelsoft.tasks.extensions.date
 import com.godelsoft.tasks.extensions.day
 import com.godelsoft.tasks.hierarchy.*
@@ -8,6 +10,8 @@ import java.util.*
 import kotlin.Comparator
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+
+class LessonInfo(val id: String, val header: String, val content: String, val description: String, val date: String, val timeBegin: String, val timeEnd: String, val type: String, val classroom: String, val teacher: String, val isNotEveryWeek: String)
 
 object DataManager {
 
@@ -26,6 +30,9 @@ object DataManager {
             onError()
         }
     }
+    val testName: String = "[{\"id\":\"1\",\"header\":\"\\u041c\\u0430\\u0442\\u0430\\u043d\\u0430\\u043b\\u0438\\u0437\",\"content\":\"description\",\"date\":\"30.08.2020\",\"timeBegin\":\"12:00\",\"timeEnd\":\"13:30\",\"type\":\"LECTURE\",\"classroom\":\"666\",\"teacher\":\"\\u0413\\u043e\\u043b\\u0443\\u0431\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"2\",\"header\":\"\\u041b\\u0438\\u043d\\u0430\\u043b\",\"content\":\"description\",\"date\":\"30.08.2020\",\"timeBegin\":\"13:40\",\"timeEnd\":\"15:10\",\"type\":\"SEMINAR\",\"classroom\":\"228\",\"teacher\":\"\\u0421\\u0442\\u0435\\u043f\\u0430\\u043d\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"3\",\"header\":\"\\u0424\\u0438\\u0437\\u0440\\u0430\",\"content\":\"description\",\"date\":\"30.08.2020\",\"timeBegin\":\"15:20\",\"timeEnd\":\"16:50\",\"type\":\"SEMINAR\",\"classroom\":\"\\u0421\\u041a\",\"teacher\":\"\\u0411\\u0440\\u044b\\u0437\\u0433\\u0430\\u043b\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"4\",\"header\":\"\\u0413\\u0440\\u0430\\u0444\\u0438\\u043a\\u0430\",\"content\":\"description\",\"date\":\"31.08.2020\",\"timeBegin\":\"12:00\",\"timeEnd\":\"13:30\",\"type\":\"SEMINAR\",\"classroom\":\"400\\u044e\",\"teacher\":\"\\u0412\\u0438\\u0448\\u043d\\u044f\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"5\",\"header\":\"\\u041c\\u0430\\u0442\\u0430\\u043d\\u0430\\u043b\\u0438\\u0437\",\"content\":\"description\",\"date\":\"31.08.2020\",\"timeBegin\":\"13:40\",\"timeEnd\":\"15:10\",\"type\":\"SEMINAR\",\"classroom\":\"242\",\"teacher\":\"\\u0427\\u0435\\u0442\\u0432\\u0435\\u0440\\u0438\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"6\",\"header\":\"\\u041c\\u0430\\u0442\\u0430\\u043d\\u0430\\u043b\\u0438\\u0437\",\"content\":\"description\",\"date\":\"31.08.2020\",\"timeBegin\":\"15:20\",\"timeEnd\":\"16:50\",\"type\":\"SEMINAR\",\"classroom\":\"242\",\"teacher\":\"\\u0427\\u0435\\u0442\\u0432\\u0435\\u0440\\u0438\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"7\",\"header\":\"\\u041b\\u0438\\u043d\\u0430\\u043b\",\"content\":\"description\",\"date\":\"01.09.2020\",\"timeBegin\":\"12:00\",\"timeEnd\":\"13:30\",\"type\":\"LECTURE\",\"classroom\":\"300\",\"teacher\":\"\\u0421\\u0442\\u0435\\u043f\\u0430\\u043d\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"8\",\"header\":\"\\u0413\\u0440\\u0430\\u0444\\u0438\\u043a\\u0430\",\"content\":\"description\",\"date\":\"01.09.2020\",\"timeBegin\":\"13:40\",\"timeEnd\":\"15:10\",\"type\":\"LECTURE\",\"classroom\":\"400\\u044e\",\"teacher\":\"\\u0412\\u0438\\u0448\\u043d\\u044f\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"},{\"id\":\"9\",\"header\":\"\\u0413\\u0440\\u0430\\u0444\\u0438\\u043a\\u0430\",\"content\":\"description\",\"date\":\"01.09.2020\",\"timeBegin\":\"15:20\",\"timeEnd\":\"16:50\",\"type\":\"LECTURE\",\"classroom\":\"400\\u044e\",\"teacher\":\"\\u0412\\u0438\\u0448\\u043d\\u044f\\u043a\\u043e\\u0432\",\"isNotEveryWeek\":\"0\"}]"
+    val resultate = Klaxon()
+        .parse<LessonInfo>(testName)
 
     fun getEventById(id: Int): Event? {
         return events[id]
